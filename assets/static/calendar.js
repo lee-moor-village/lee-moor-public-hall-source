be17afca7cddd9ad3cd72cd0903c3795245f0254
Simon King <simon@simonking.org.uk>
2017-01-17 21:21:14 +0000

    calendar.js: improve date formatting

--- assets/static/calendar.js
+++ assets/static/calendar.js
@@ -5,6 +5,30 @@
         var pad = function(n) {
             return n < 10 ? '0' + n : n;
         };
+        var parsedate = function(datestring) {
+            var parts = datestring.match(/([0-9]{4})-([0-9]{2})-([0-9]{2})/);
+            var date = new Date(parseInt(parts[1]),
+                                parseInt(parts[2]) - 1,
+                                parseInt(parts[3]));
+            return date;
+        };
+        var formatitemdate = function(item) {
+            if (item.start.date) {
+                return parsedate(item.start.date).toDateString();
+            } else {
+                var parts = item.start.dateTime.split('T');
+                var date = parsedate(parts[0]);
+                var timeparts = parts[1].match(/([0-9]{2}):([0-9]{2}):([0-9]{2})/);
+                date.setUTCHours(parseInt(timeparts[1]));
+                date.setUTCMinutes(parseInt(timeparts[2]));
+                date.setUTCSeconds(parseInt(timeparts[3]));
+                return (date.toDateString()
+                        + ' '
+                        + pad(date.getHours())
+                        + ':'
+                        + pad(date.getMinutes()));
+            };
+        };
         gapi.load('client', function() {
             gapi.client.setApiKey(options.apikey);
             gapi.client.load('calendar', 'v3').then(function() {
@@ -29,7 +53,7 @@
                                   '<div class="gcal-event-summary"></div>' +
                                   '<div class="gcal-event-description"></div>' +
                                   '</div>');
-                    $node.find('.gcal-event-date').text(item.start.date || item.start.dateTime);
+                    $node.find('.gcal-event-date').text(formatitemdate(item));
                     $node.find('.gcal-event-summary').text(item.summary);
                     var $descnode = $node.find('.gcal-event-description');
                     if (item.description) {
